61dfe62da37824236a9d68dadcc8943d600f2e5d
aTrotier <a.trotier@gmail.com>
2022-08-26 13:17:48 +0200

    build bart and test in julia

--- test/testIO.jl
+++ test/testIO.jl
@@ -27,24 +27,7 @@ end
 @testset "BART" begin
     pathtobart = "/home/runner/work/BartIO.jl/BartIO.jl/bart"
 
-    if Sys.isapple()
-        @warn "BART execution is currently not tested on macOS version"
-
-        @info "test pycall setup"
-        python_pycall = PyCall.python
-        run(`$python_pycall -m pip install numpy`)
-        PyCall.py"""
-        import os
-        import sys
-        os.environ['TOOLBOX_PATH'] = $pathtobart
-        path = os.environ["TOOLBOX_PATH"] + "/python/"
-        sys.path.append(path)
-        """
-
-    else
-        @info "test whole wrapper"
-        bart = BartIO.wrapperBart(pathtobart)
-        phant = bart(1,"phantom")
-        @test size(phant) == (128, 128)
-    end
+    bart = BartIO.wrapperBart(pathtobart)
+    phant = bart(1,"phantom")
+    @test size(phant) == (128, 128)
 end
